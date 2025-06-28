2cd6b0f1d4a957f555c3219310521bff64a4bbc5
AyushxOD <ayushroy0108@gmail.com>
2025-06-28 21:35:41 +0530

    fix: Add TypeScript types for three.js

--- server/src/main.ts
+++ server/src/main.ts
@@ -7,8 +7,12 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   // V V V V V  ADD THIS LINE RIGHT HERE  V V V V V
-  app.enableCors();
-  // ^ ^ ^ ^ ^  THIS IS THE PERMISSION SLIP  ^ ^ ^ ^ ^
+  app.enableCors({
+    origin: process.env.FRONTEND_URL, // We will set this on Render
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    credentials: true,
+  });
+    // ^ ^ ^ ^ ^  THIS IS THE PERMISSION SLIP  ^ ^ ^ ^ ^
 
   await app.listen(process.env.PORT || 3001);}
 bootstrap();
